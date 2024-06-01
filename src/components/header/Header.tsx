5c52364f8f4d8892e12a197be9017f02a292b158
Danila P <danila.ep@gmail.com>
2024-06-01 13:47:38 +0300

    start summer sale

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,16 +14,16 @@ export default function Header() {
         <BurgerMenu />
 
         <div className='w-full h-full flex justify-center flex-col items-center'>
-          <div className='text-gray-50 text-6xl xl:text-9xl font-semibold fursNeue leading-[3.3rem] mt-[148px] text-center uppercase'>
-            Закрытая
+          <div className='text-gray-50 text-4xl xl:text-9xl font-semibold fursNeue leading-[3.3rem] mt-[148px] text-center uppercase'>
+            summer
             <br />
-            распродажа
+            sale
           </div>
           <div className='text-lime-300 text-xl xl:text-5xl font-medium fursNeue'>
             СКИДКА ДО 50%
           </div>
           <div className='text-white fursNeue text-sm xl:text-4xl mb-[91px]'>
-            с 25 по 31 мая
+            с 1 по 30 июня
           </div>
         </div>
       </div>
